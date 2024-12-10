4dc9b790faaaa6ef05fe8afe3a4569d235a91ca6
Dotori0309 <86943734+Dotori0309@users.noreply.github.com>
2024-12-10 14:18:36 +0900

    Algorithm dynamic programming method example
    Algorithm dynamic programming method example

---
+++ Algorithm dynamic programming method/실습02_피보나치(DP-수행시간).py
@@ -0,0 +1,25 @@
+## DP로 구현한 피보나치 수열의 성능측정
+import time
+
+n = int(input("n을 입력하세요:"))
+dictionary = { 1: 1, 2: 1}
+counter = 0
+
+def fibo(n):
+    global counter
+    counter += 1
+    if n in dictionary:
+        return dictionary[n]
+    else:
+        output = fibo(n-1) + fibo(n-2)
+        dictionary[n]=output
+        return output
+
+start = time.time()
+fibo(n)
+end = time.time()
+
+print("---------------------------------------------------------------")
+print(f"fibo({n}) 계산에 활용된 덧셈 횟수는 {counter}번입니다.") 
+print("실행시간=", end - start)
+# print(dictionary.items())
